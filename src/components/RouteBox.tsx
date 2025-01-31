f21e8845b3d2ba5fb7e1f3a87b51a44caa23e201
ThoughtVessel <50349667+ThoughtVessel@users.noreply.github.com>
2025-01-31 11:38:16 -0800

    Made route box bigger on mobile and added "Due"

--- src/components/RouteBox.tsx
+++ src/components/RouteBox.tsx
@@ -8,17 +8,19 @@ interface RouteBoxProps {
 
 export function RouteBox({ id, description, times, color }: RouteBoxProps) {
   //If you want to replace zero times with a word, modify here.
-  //const formattedTimes = times.map(time => time === 0 ? 'Now' : time);
-  const formattedTimes = times;
+  const formattedTimes = times.map(time => time === 0 ? 'Due' : time);
+  //const formattedTimes = times;
   return (
     <div className={`flex items-center justify-between w-full px-4 sm:px-8 py-4 sm:py-6 ${color}`}>
       <div className="flex flex-col gap-1">
         <span className="text-white text-3xl sm:text-5xl font-bold">{id}</span>
         <span className="sm:block text-white text-base sm:text-2xl">{description}</span>
       </div>
-      <div className="flex flex-col items-center justify-center min-w-[8rem] sm:min-w-[12rem]">
+      <div className="flex flex-col items-center justify-center min-w-[8rem] sm:min-w-[16rem]">
         <div className="text-white text-3xl sm:text-5xl font-medium">
-          {formattedTimes.join(', ')}
+
+          {'45'}
+          {/*formattedTimes.join(', ')*/}
         </div>
         <span className="text-white/90 text-base sm:text-lg">minutes</span>
       </div>
